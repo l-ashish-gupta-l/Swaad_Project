91a7cefb65d05e510faf5afe0ab4d74cbf0f51dd
l-ashish-gupta-l <ashish3072002@gmail.com>
2024-04-17 11:27:25 +0530

    react-hook-form on booking table

--- Client/src/Pages/Booking.jsx
+++ Client/src/Pages/Booking.jsx
@@ -3,33 +3,22 @@ import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import MenuPAGEBg from '../assets/MenuPAGEBg.jpg'
 import axios from 'axios';
+import { useForm } from 'react-hook-form';
 function Booking() {
+    const form = useForm()
+    const { register, handleSubmit, formState } = form
+    const { errors } = formState
 
-    const [Details, setDetails] = useState({
-        name: "",
-        email: "",
-        phone: "",
-        bookingDate: "",
-        time: "",
-        numberOfPeople: " "
-    })
-
-    const handleChange = (e) => {
-        setDetails({ ...Details, [e.target.name]: e.target.value })
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(Details);
-        axios.post('http://localhost:3000/create', Details)
+    const onSubmit = (data) => {
+        console.log(data);
+        axios.post('http://localhost:3000/create', data)
             .then(response => {
                 console.log(response.data);
             })
             .catch(error => {
                 console.error('There was an error!', error);
             });
-    };
-
+    }
 
     return (
         <>
@@ -42,54 +31,75 @@ function Booking() {
             <div className='content'>
                 <div className="flex items-center justify-center p-12">
                     <div className="mx-auto w-full max-w-[550px] bg-white">
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={handleSubmit(onSubmit)} noValidate >
                             <div className="mb-5">
-                                <label for="name" className="mb-3 block text-base font-medium text-[#07074D]">
+                                <label htmlFor="name" className="mb-3 block text-base font-medium text-[#07074D]">
                                     Full Name
                                 </label>
-                                <input type="text" name="name" id="name" placeholder="Full Name" onChange={handleChange}
+                                <input type="text" id="name" placeholder="Full Name" {...register("name", {
+                                    required: "Username is required",
+                                })}
                                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#000000] focus:shadow-md" />
+                                <p className='text-red-600 font-semibold'>{errors.name?.message}</p>
                             </div>
                             <div className="mb-5">
-                                <label for="phone" className="mb-3 block text-base font-medium text-[#07074D]">
+                                <label htmlFor="phone" className="mb-3 block text-base font-medium text-[#07074D]">
                                     Phone Number
                                 </label>
-                                <input type="text" name="phone" id="phone" placeholder="Enter your phone number" onChange={handleChange}
+                                <input type="text" {...register("phone", {
+                                    required: "Phone number is required",
+                                    pattern: {
+                                        value: /^(\+\d{1,3}[- ]?)?\d{10}$/,
+                                        message: "Invalid phone number format"
+                                    }
+                                })} id="phone" placeholder="Enter your phone number"
                                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#000000] focus:shadow-md" />
+                                <p className='text-red-600 font-semibold'>{errors.phone?.message}</p>
                             </div>
                             <div className="mb-5">
-                                <label for="email" className="mb-3 block text-base font-medium text-[#07074D]">
+                                <label htmlFor="email" className="mb-3 block text-base font-medium text-[#07074D]">
                                     Email Address
                                 </label>
-                                <input type="email" name="email" id="email" placeholder="Enter your email" onChange={handleChange}
+                                <input type="email" {...register("email", {
+                                    required: "Email is required",
+                                    pattern: {
+                                        value: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+                                        message: "Email format is incorrect."
+                                    }
+                                })
+
+                                } id="email" placeholder="Enter your email"
                                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#000000] focus:shadow-md" />
+                                <p className='text-red-600 font-semibold'>{errors.email?.message}</p>
                             </div>
                             <div className="-mx-3 flex flex-wrap">
                                 <div className="w-full px-3 sm:w-1/2">
                                     <div className="mb-5">
-                                        <label for="date" className="mb-3 block text-base font-medium text-[#07074D]">
+                                        <label htmlFor="date" className="mb-3 block text-base font-medium text-[#07074D]">
                                             Date
                                         </label>
-                                        <input type="date" name="bookingDate" id="date" onChange={handleChange}
+                                        <input type="date" {...register("bookingDate")} id="date"
                                             className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#000000] focus:shadow-md" />
                                     </div>
                                 </div>
                                 <div className="w-full px-3 sm:w-1/2">
                                     <div className="mb-5">
-                                        <label for="time" className="mb-3 block text-base font-medium text-[#07074D]">
+                                        <label htmlFor="time" className="mb-3 block text-base font-medium text-[#07074D]">
                                             Time
                                         </label>
-                                        <input type="time" name="bookingTime" id="time" onChange={handleChange}
+                                        <input type="time" id="time" {...register("bookingTime")}
                                             className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#000000] focus:shadow-md" />
                                     </div>
                                 </div>
                             </div>
                             <div className="mb-5">
-                                <label for="no_of_person" className="mb-3 block text-base font-medium text-[#07074D]">
+                                <label htmlFor="no_of_person" className="mb-3 block text-base font-medium text-[#07074D]">
                                     Number of Persons
                                 </label>
-                                <input type="number" name="numberOfPeople" id="no_of_person" placeholder="Number of Persons" onChange={handleChange}
+                                <input type="number" {...register("numberOfPeople")} id="no_of_person" placeholder="Number of Persons"
                                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#000000] focus:shadow-md" />
+                                <p className='text-red-600 font-semibold'>{errors.numberOfPeople?.message}</p>
+
                             </div>
                             <div>
                                 <button
